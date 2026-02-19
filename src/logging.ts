5b0d28947269c6487daf8604af377516adf08f41
davidvargas <davidvargas.d45@gmail.com>
2026-02-19 01:30:35 -0500

    chore: remove legacy logging module

--- src/logging.ts
+++
@@ -1,7 +0,0 @@
-export function safeLog(event: string, data: Record<string, unknown> = {}) {
-  try {
-    console.log(JSON.stringify({ event, ...data }));
-  } catch {
-    console.log(`[${event}]`, data);
-  }
-}
